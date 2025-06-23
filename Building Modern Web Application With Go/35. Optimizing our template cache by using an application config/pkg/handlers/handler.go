ba8f4060c6923c7a8ea3ac0e4908494bb7c6e5ac
Suramethar Chantarasmee <hlek@live.com>
2025-06-23 18:22:40 +0700

    golang

---
+++ Building Modern Web Application With Go/35. Optimizing our template cache by using an application config/pkg/handlers/handler.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"myApp/pkg/render"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.tmpl")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "about.page.tmpl")
+}
+
+
+// This is handler.go file that are in render folder.
